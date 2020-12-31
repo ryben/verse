57af5a723ffac2c6b7322a177b03d65f33dff8e4
Rey <reybenedicto06@gmail.com>
2020-12-31 11:17:38 +0800

    Parsing of bookname Prepare booknames json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,9 @@
       <input v-model="verseAddressInput"/>
       <button @click="onClickGo">Go</button>
     </div>
+    <div id="errorDisplay">
+      {{ errorDisplay }}
+    </div>
     <br/>
     <div id="verseTitle">
       {{ verseTitle }}
@@ -29,16 +32,27 @@ export default {
   data: function() {
     return {
       bible: {},
+      bookNames: ['Genesis', 'Exodo', 'Levitico', 'Bilang', 'Deuteronomio'],
       verseAddressInput: 'Genesis 1:1',
       verseTitle: 'Genesis 1:1',
       verseTranslation: 'Ang Dating Biblia',
-      verseContent: 'Nang pasimula ay...'
+      verseContent: 'Nang pasimula ay...',
+      errorDisplay: null
     }
+  },
+  mounted: function() {
   },
   methods: {
     onClickGo: function() {
-      let verseAddress = this.parseVerseInput(this.verseAddressInput)
-      this.fetchVerseContent(verseAddress, this.displayVerseContent)
+      this.errorDisplay = null
+      try {
+        let verseAddress = this.parseVerseInput(this.verseAddressInput)
+
+        this.fetchVerseContent(verseAddress, this.displayVerseContent)
+        this.verseTitle = this.bookNames[verseAddress.book - 1] + ' ' + verseAddress.chapter + ':' + verseAddress.verse
+      } catch (error) {
+        this.errorDisplay = error
+      }
     },
     parseVerseInput: function(verseInput) {
       let verseAddressRegex =
@@ -49,17 +63,38 @@ export default {
                         + "[ \\.:]" // chapter-verse separator
                         + "(\\d+)") // verse
       let matchGroups = verseInput.match(verseAddressRegex)
+      let bookInput = matchGroups[2]
+
+      let bookMatch = this.findBookMatch(bookInput)
+
       return {
-        'book': '1', //matchGroups[2],
+        'book': bookMatch, //matchGroups[2],
         'chapter': matchGroups[3] + '',
         'verse': matchGroups[4] + ''
+      }
+    },
+    findBookMatch(bookName) {
+      let bookMatches = []
+
+      for (let i=0 ; i<this.bookNames.length ; i++) {
+        if (this.bookNames[i].substring(0, bookName.length).toLowerCase() == bookName.toLowerCase()) {
+          bookMatches.push(i)
+        }
+      }
+
+      if (bookMatches.length == 1) {
+        return bookMatches[0] + 1 // offset of 1 since book number starts with 1
+      } else {
+        throw 'Invalid book name entered'
       }
     },
     fetchVerseContent: function(verseAddress, callback) {
       let baseUrl = 'https://ryben.github.io/verse/verses/'
 
       if (!this.bible[verseAddress.book]) {
-        axios.get(baseUrl + verseAddress.book + '.json').then(response => {
+        let fetchUrl = baseUrl + verseAddress.book + '.json'
+        console.log(fetchUrl)
+        axios.get(fetchUrl).then(response => {
           this.bible[verseAddress.book] = response.data // cache the fetched book content
           callback(verseAddress)
         })
