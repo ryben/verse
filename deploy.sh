fc2905c25dd806c003741c6e76907c8daa821029
Rey Benedicto <reybenedicto06@gmail.com>
2024-02-13 14:36:44 +0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
+
+export NODE_OPTIONS=--openssl-legacy-provider
+
 # build
 npm run build
 
-cp -r verses dist
+# cp -r verses dist
 
 # navigate into the build output directory
 cd dist
@@ -13,5 +16,8 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:ryben/verse.git master:gh-pages
+git push -f git@github.com:ryben/verse.git main:gh-pages
+
+unset NODE_OPTIONS
+
 cd -
